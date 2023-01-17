5fa9e7799d4c1a45f669b1ef3736c7c2b8c9e43d
alexander papageorge <atpapag@gmail.com>
2023-01-16 19:12:43 -0500

    something more like writer

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,70 @@
 use std::rc::Rc;
 
 mod writer;
-
-use writer::{WriterKleisli, WriterMonad, writer_unit, writer_bind, compose_writers};
+use writer::{WriterKleisli, WriterMonad, writer_unit, writer_bind, compose_writers, log, StringLog};
 
 fn main() {
 
-    fn add1_function(x: i64) -> WriterMonad<i64> {
-        return (x + 1, "added 1".to_string())
+    fn add1_function(x: i64) -> WriterMonad<i64, StringLog> {
+        return (x + 1, StringLog { log: "added 1".to_string() })
     }
     let add1 = WriterKleisli { kleisli: Rc::new(add1_function) };
 
-    let times2 = WriterKleisli { kleisli: Rc::new(|x: i64| -> WriterMonad<i64> { (2 * x, "multiplied by 2".to_string()) })};
+    let times2 = WriterKleisli { kleisli: Rc::new(|x: i64| -> WriterMonad<i64, StringLog> {
+        (2 * x, StringLog { log: "multiplied by 2".to_string() })
+    })};
 
-    let add1_times2: WriterKleisli<i64, i64> = compose_writers(add1, times2);
+    let add1_times2: WriterKleisli<i64, i64, StringLog> = compose_writers(add1, times2);
 
-    let val_log: WriterMonad<i64> = writer_bind(writer_unit(10), add1_times2);
+    let val_log: WriterMonad<i64, StringLog> = writer_bind(writer_unit(10), add1_times2);
 
-    println!("{}", val_log.1);
+    println!("{}", val_log.1.log);
 
-    // let m0 = writer_unit(11);
-    let k0 = WriterKleisli { kleisli: Rc::new(
-        |x: i64| -> WriterMonad<i64> { (3*x, "x3".to_string()) }
+
+    let k0: WriterKleisli<i64, i64, StringLog> = WriterKleisli { kleisli: Rc::new(
+        |x: i64| -> WriterMonad<i64, StringLog> { (3*x, StringLog { log: "x3".to_string() }) }
     ) };
     let k1 = k0.clone();
-    let result = writer_binds!( { writer_unit(11) } >>= { k0 } >>= { k1 });
+    let k2 = k0.clone();
+    let k3 = k0.clone();
+    let k4: WriterKleisli<i64, f64, StringLog> = WriterKleisli { kleisli: Rc::new(
+        |x: i64| -> WriterMonad<f64, StringLog> { ((x as f64) / 10., StringLog { log: "div10".to_string() }) }
+    ) };
+    let result = writer_binds!( { writer_unit(11) } >>= { k0 } >>= { k1 } >>= { k2 } >>= { k3 } >>= { k4 } );
     println!("{:?}", result);
+
+
+    let f = |x| writer_do!(
+        y = 4*x + 7,
+        z = y * 10,
+        z
+    );
+    println!("{:?}", f(3));
+
+
+    let g = |x, y| writer_do!(
+        x0 = 4 * x + 7,
+        x1 = y * 10,
+        x2 = x0 + x1,
+        x2
+    );
+    println!("{:?}", g(3, 8));
+
+
+    let square_value = |ma: (i64, StringLog) | writer_do!(
+        val <- ma,
+        val2 = val * val,
+        log(StringLog{ log: "squared the number".to_string() }),
+        writer_unit(val2)
+    );
+    println!("{:?}", square_value(writer_unit(13)));
+
+
+    let add_values = |x: i64| writer_do!(
+        log("adding numbers now!".to_string()),
+        val = (1..x).sum::<i64>(),
+        log("finished adding numbers!".to_string()),
+        writer_unit(val)
+    );
+    println!("{:?}", add_values(30));
 }
--- src/writer.rs
+++ src/writer.rs
@@ -1,57 +1,127 @@
 use std::fmt::Display;
 use std::rc::Rc;
 
-pub type WriterMonad<Ta> = (Ta, String);
+mod monoid;
+use monoid::{Monoid};
+
+#[derive(Debug, Clone)]
+pub struct StringLog {
+    pub log: String
+}
+
+impl Monoid for StringLog {
+    type T = StringLog;
+    fn mempty() -> Self::T { StringLog { log: "".to_string() } }
+    fn mappend(&self, other: Self::T) -> Self::T { StringLog { log: format!("{}\n{}", self.log, other.log) } }
+}
+
+impl Monoid for String {
+    type T = String;
+    fn mempty() -> Self::T { "".to_string() }
+    fn mappend(&self, other: Self::T) -> Self::T { format!("{}\n{}", self, other) }
+}
 
 
-pub fn writer_unit<Ta: Display + Copy>(a: Ta) -> WriterMonad<Ta> {
-    (a, format!("value is {}", a))
+pub type WriterMonad<Ta, Tlog> = (Ta, Tlog);
+
+
+pub fn writer_unit<Ta: Display + Copy, Tlog: Monoid<T = Tlog>>(a: Ta) -> WriterMonad<Ta, Tlog> {
+    (a, <Tlog as Monoid>::mempty())
 }
 
 
 // functor
-fn writer_fmap<Ta, Tb>(f_ab: fn(Ta) -> Tb, ma: WriterMonad<Ta>) -> WriterMonad<Tb> {
+fn writer_fmap<Ta, Tb, Tlog: Monoid>(f_ab: fn(Ta) -> Tb, ma: WriterMonad<Ta, Tlog>) -> WriterMonad<Tb, Tlog> {
     (f_ab(ma.0), ma.1)
 }
 
 
 // applicative
-fn writer_applicative<Ta, Tb>(
-    mf: WriterMonad<fn(Ta) -> Tb>,
-    ma: WriterMonad<Ta>
-) -> WriterMonad<Tb> {
-    ((mf.0)(ma.0), format!("{}\n{}", mf.1, ma.1))
+fn writer_applicative<Ta, Tb, Tlog: Monoid<T = Tlog>>(
+    mf: WriterMonad<fn(Ta) -> Tb, Tlog>,
+    ma: WriterMonad<Ta, Tlog>
+) -> WriterMonad<Tb, Tlog> {
+    ((mf.0)(ma.0), mf.1.mappend(ma.1))
 }
 
 
 // monad
 #[derive(Clone)]
-pub struct WriterKleisli<Ta, Tb> {
-    pub kleisli: Rc<dyn Fn(Ta) -> WriterMonad<Tb>>
+pub struct WriterKleisli<Ta, Tb, Tlog: Monoid> {
+    pub kleisli: Rc<dyn Fn(Ta) -> WriterMonad<Tb, Tlog>>
 }
 
-pub fn writer_bind<Ta, Tb>(ma: WriterMonad<Ta>, k_ab: WriterKleisli<Ta, Tb>) -> WriterMonad<Tb> {
+pub fn writer_bind<Ta, Tb, Tlog: Monoid<T = Tlog>>(
+    ma: WriterMonad<Ta, Tlog>,
+    k_ab: WriterKleisli<Ta, Tb, Tlog>
+) -> WriterMonad<Tb, Tlog> {
     let b_log_ab = (k_ab.kleisli)(ma.0);
-    (b_log_ab.0, format!("{}\n{}", ma.1, b_log_ab.1))
+    (b_log_ab.0, ma.1.mappend(b_log_ab.1))
+}
+
+pub fn log<Tlog: Monoid>(msg: Tlog) -> WriterMonad<(), Tlog> {
+    ((), msg)
 }
 
 #[macro_export]
 macro_rules! writer_binds {
     ($m:block) => { $m };
     ($m:block >>= $k:block) => { writer_bind($m, $k) };
-    ($m:block >>= $k:block >>= $rest:tt) => { writer_binds!({ writer_bind($m, $k) } >>= $rest) };
+    ($m:block >>= $k:block >>= $($rest:tt)*) => { writer_binds!({ writer_bind($m, $k) } >>= $($rest)* ) };
+}
+
+#[macro_export]
+macro_rules! writer_do {
+    ($v:ident = $e:expr,  $($rest:tt)*) => { (|$v| { writer_do!($($rest)*) })($e) };
+    ($v:ident = $b:block, $($rest:tt)*) => { (|$v| { writer_do!($($rest)*) })($b) };
+
+    ($v:ident <- $e:expr, $($rest:tt)*) => {
+        writer_bind(
+            $e,
+            WriterKleisli {
+                kleisli: Rc::new( move |$v| { writer_do!($($rest)*) } )
+            }
+        )
+    };
+    ($v:ident <- $b:block, $($rest:tt)*) => {
+        writer_bind(
+            $b,
+            WriterKleisli {
+                kleisli: Rc::new( move |$v| { writer_do!($($rest)*) } )
+            }
+        )
+    };
+
+    ($e:expr, $($rest:tt)*) => {
+        writer_bind(
+            $e,
+            WriterKleisli {
+                kleisli: Rc::new( move |_| { writer_do!($($rest)*) } )
+            }
+        )
+    };
+    ($b:block, $($rest:tt)*) => {
+        writer_bind(
+            $b,
+            WriterKleisli {
+                kleisli: Rc::new( move |_| { writer_do!($($rest)*) } )
+            }
+        )
+    };
+
+    ($e:expr) => { $e };
 }
 
 
-pub fn compose_writers<Ta: 'static, Tb: 'static, Tc: 'static>(
-    wab: WriterKleisli<Ta, Tb>, wbc: WriterKleisli<Tb, Tc>
-) -> WriterKleisli<Ta, Tc> {
+pub fn compose_writers<Ta: 'static, Tb: 'static, Tc: 'static, Tlog: Monoid<T = Tlog> + 'static>(
+    wab: WriterKleisli<Ta, Tb, Tlog>, wbc: WriterKleisli<Tb, Tc, Tlog>
+) -> WriterKleisli<Ta, Tc, Tlog> {
 
     WriterKleisli {
-        kleisli: Rc::new(move |a: Ta| -> WriterMonad<Tc> {
+        kleisli: Rc::new(move |a: Ta| -> WriterMonad<Tc, Tlog> {
             let b_log_ab = (wab.kleisli)(a);
             let c_log_bc = (wbc.kleisli)(b_log_ab.0);
-            (c_log_bc.0, format!("{}\n{}", b_log_ab.1, c_log_bc.1))
+            (c_log_bc.0, b_log_ab.1.mappend(c_log_bc.1))
         })
     }
 
