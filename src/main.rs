b00d2f9280ee00cf0ed566b2e69bdd0b23f82682
alexander papageorge <atpapag@gmail.com>
2023-01-22 16:49:52 -0500

    tests in modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::i64;
 use std::rc::Rc;
 
 mod monads;
@@ -7,93 +8,6 @@ use crate::monads::state::{StateKleisli, StateMonad, state_unit, state_fmap, sta
 
 
 fn main() {
-    ///////////////////// Writer //////////////////////////////////
-
-    assert_eq!(
-        writer_fmap(|x| 2 * x, (5, "hello".to_string())),
-        (10, "hello".to_string()));
-    assert_eq!(
-        writer_fmap(|x| if x == 5 { "zero" } else { "one" }, (5, "hello".to_string())),
-        ("zero", "hello".to_string())
-    );
-    assert_eq!(
-        writer_apply((|x| 2*x, "hello".to_string()), (3, "goodbye".to_string())),
-        (6, "hello\ngoodbye".to_string())
-    );
-    assert_eq!(
-        writer_bind(
-            (1, "hello".to_string()),
-            WriterKleisli { kleisli: Rc::new( |x| (2*x, "goodbye".to_string())) }
-        ),
-        (2, "hello\ngoodbye".to_string())
-    );
-
-    let do_calculation = |x| writer_do!(
-        log(format!("received number {}", x)),
-        x0 = x + 10,
-        log("added 10 to the number".to_string()),
-        x1 = 2 * x0,
-        log("multiplied result by 2".to_string()),
-        writer_unit(x1)
-    );
-    assert_eq!(
-        do_calculation(5),
-        (30, "received number 5\nadded 10 to the number\nmultiplied result by 2\n".to_string())
-    );
-
-    ///////////////////// Reader //////////////////////////////////
-
-    let r0: ReaderMonad<&str, i64> = reader_fmap(
-        Rc::new(|x: i64| 3*x),
-        ReaderMonad { run_reader: Rc::new(|cfg| 11) }
-    );
-    let r1: ReaderMonad<&str, i64> = ReaderMonad{ run_reader: Rc::new(|cfg| 33) };
-    assert_eq!((r0.run_reader)("hi"), (r1.run_reader)("hi"));
-
-    let r0: ReaderMonad<&str, i64> = reader_fmap(
-        Rc::new(|x: i64| 3*x),
-        ReaderMonad { run_reader: Rc::new(|cfg| if cfg == "one" { 10 } else { 1 }) }
-    );
-    assert_eq!((r0.run_reader)("one"), 30);
-    assert_eq!((r0.run_reader)("none"), 3);
-
-    let r0: ReaderMonad<&str, Rc<dyn Fn(i64) -> i64>> = ReaderMonad {
-        run_reader: Rc::new( |cfg| {Rc::new(move |x| if cfg == "one" { x } else { 2 * x })})
-    };
-    let r1: ReaderMonad<&str, i64> = ReaderMonad { run_reader: Rc::new( |cfg| 10 )};
-    let r2 = reader_apply(r0, r1);
-    assert_eq!((r2.run_reader)("one"), 10);
-    assert_eq!((r2.run_reader)("two"), 20);
-
-    let r0: ReaderMonad<(bool, bool), &str> = ReaderMonad { run_reader: Rc::new(|cfg| if cfg.0 { "t" } else { "f" })};
-    let k0: ReaderKleisli<(bool, bool), &str, (&str, &str)> = ReaderKleisli {
-        kleisli: Rc::new( |l|
-            ReaderMonad { run_reader: Rc::new( |cfg| if cfg.1 { (l, "t") } else { (l, "f") } ) }
-        )};
-    let r1 = reader_bind(r0, k0);
-    assert_eq!((r1.run_reader)((true, true)), ("t", "t"));
-    assert_eq!((r1.run_reader)((true, false)), ("t", "f"));
-    assert_eq!((r1.run_reader)((false, true)), ("f", "t"));
-    assert_eq!((r1.run_reader)((false, false)), ("f", "f"));
-
-    #[derive(Debug, Clone)]
-    struct User {
-        uname: String,
-        host: String,
-        ip: String,
-    }
-    let gen_login_str: ReaderMonad<User, String> = reader_do!(
-        cfg <- load(),
-        reader_unit(format!("{}-{}-{}", cfg.uname, cfg.host, cfg.ip))
-    );
-    assert_eq!(
-        (gen_login_str.run_reader)(User { uname: "a".to_string(), host: "b".to_string(), ip: "c".to_string() }),
-        "a-b-c".to_string()
-    );
-
-    ///////////////////// State //////////////////////////////////
-    
-
 
     fn add1_function(x: i64) -> WriterMonad<i64, StringLog> {
         return (x + 1, StringLog { log: "added 1".to_string() })
