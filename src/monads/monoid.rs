e8f78e11be4beab47dad3941d450fdc3cd9c94ea
alexander papageorge <alexander.papageorge@openlabsusa.org>
2023-01-24 00:09:04 -0500

    cleaner

--- src/monads/monoid.rs
+++ src/monads/monoid.rs
@@ -1,5 +1,30 @@
+// A monoid is defined by a type T of which it
+// returns an `empty` version thereof
+// can concatenate onto itself.
 pub trait Monoid {
     type T;
     fn mempty() -> Self::T;
     fn mappend(&self, other: Self::T) -> Self::T;
+}
+
+
+// containers, in general, are monoids
+
+// strings
+impl Monoid for String {
+    type T = String;
+    fn mempty() -> Self::T { "".to_string() }
+    fn mappend(&self, other: Self::T) -> Self::T { format!("{}\n{}", self, other) }
+}
+
+// lists
+impl<A: Clone> Monoid for Vec<A> {
+    type T = Vec<A>;
+    fn mempty() -> Self::T { vec![] }
+    fn mappend(&self, other: Self::T) -> Self::T {
+        let mut ret: Self::T = vec![];
+        for el in self { ret.push(el.clone()); }
+        for el in other { ret.push(el.clone()); }
+        return ret
+    }
 }
--- src/monads/writer.rs
+++ src/monads/writer.rs
@@ -3,24 +3,9 @@ use std::rc::Rc;
 
 use super::monoid::{Monoid};
 
-#[derive(Debug, Clone)]
-pub struct StringLog {
-    pub log: String
-}
 
-impl Monoid for StringLog {
-    type T = StringLog;
-    fn mempty() -> Self::T { StringLog { log: "".to_string() } }
-    fn mappend(&self, other: Self::T) -> Self::T { StringLog { log: format!("{}\n{}", self.log, other.log) } }
-}
-
-impl Monoid for String {
-    type T = String;
-    fn mempty() -> Self::T { "".to_string() }
-    fn mappend(&self, other: Self::T) -> Self::T { format!("{}\n{}", self, other) }
-}
-
-
+// A straightforward representation of the writer monad.
+// Any joining of an object and a log [monoid] will suffice.
 pub type WriterMonad<Ta, Tlog> = (Ta, Tlog);
 
 
@@ -30,7 +15,10 @@ pub fn writer_unit<Ta: Display + Copy, Tlog: Monoid<T = Tlog>>(a: Ta) -> WriterM
 
 
 // functor
-pub fn writer_fmap<Ta, Tb, Tlog: Monoid>(f_ab: fn(Ta) -> Tb, ma: WriterMonad<Ta, Tlog>) -> WriterMonad<Tb, Tlog> {
+pub fn writer_fmap<Ta, Tb, Tlog: Monoid>(
+    f_ab: fn(Ta) -> Tb,
+    ma: WriterMonad<Ta, Tlog>
+) -> WriterMonad<Tb, Tlog> {
     (f_ab(ma.0), ma.1)
 }
 
@@ -54,13 +42,16 @@ pub fn writer_bind<Ta, Tb, Tlog: Monoid<T = Tlog>>(
     ma: WriterMonad<Ta, Tlog>,
     k_ab: WriterKleisli<Ta, Tb, Tlog>
 ) -> WriterMonad<Tb, Tlog> {
-    let b_log_ab = (k_ab.kleisli)(ma.0);
+    let b_log_ab: WriterMonad<Tb, Tlog> = (k_ab.kleisli)(ma.0);
     (b_log_ab.0, ma.1.mappend(b_log_ab.1))
 }
 
+
+// pushes a message into the log.
 pub fn log<Tlog: Monoid>(msg: Tlog) -> WriterMonad<(), Tlog> {
     ((), msg)
 }
+
 
 #[macro_export]
 macro_rules! writer_binds {
@@ -68,6 +59,23 @@ macro_rules! writer_binds {
     ($m:block >>= $k:block) => { writer_bind($m, $k) };
     ($m:block >>= $k:block >>= $($rest:tt)*) => { writer_binds!({ writer_bind($m, $k) } >>= $($rest)* ) };
 }
+
+
+pub fn compose_writers<Ta: 'static, Tb: 'static, Tc: 'static, Tlog: Monoid<T = Tlog> + 'static>(
+    wab: WriterKleisli<Ta, Tb, Tlog>,
+    wbc: WriterKleisli<Tb, Tc, Tlog>
+) -> WriterKleisli<Ta, Tc, Tlog> {
+
+    WriterKleisli {
+        kleisli: Rc::new(move |a: Ta| -> WriterMonad<Tc, Tlog> {
+            let b_log_ab: WriterMonad<Tb, Tlog> = (wab.kleisli)(a);
+            let c_log_bc: WriterMonad<Tc, Tlog> = (wbc.kleisli)(b_log_ab.0);
+            (c_log_bc.0, b_log_ab.1.mappend(c_log_bc.1))
+        })
+    }
+
+}
+
 
 #[macro_export]
 macro_rules! writer_do {
@@ -92,21 +100,6 @@ macro_rules! writer_do {
     };
 
     ($e:expr) => { $e };
-}
-
-
-pub fn compose_writers<Ta: 'static, Tb: 'static, Tc: 'static, Tlog: Monoid<T = Tlog> + 'static>(
-    wab: WriterKleisli<Ta, Tb, Tlog>, wbc: WriterKleisli<Tb, Tc, Tlog>
-) -> WriterKleisli<Ta, Tc, Tlog> {
-
-    WriterKleisli {
-        kleisli: Rc::new(move |a: Ta| -> WriterMonad<Tc, Tlog> {
-            let b_log_ab = (wab.kleisli)(a);
-            let c_log_bc = (wbc.kleisli)(b_log_ab.0);
-            (c_log_bc.0, b_log_ab.1.mappend(c_log_bc.1))
-        })
-    }
-
 }
 
 
